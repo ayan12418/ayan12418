a3700f278af1850f1edc168c4d33c1817efafda5
Mohammad ayan <ayanansari745@gmail.com>
2026-01-04 20:45:08 +0530

    Inital code added

---
+++ index.js
@@ -0,0 +1,11 @@
+const age = 24;
+const name = 'Mohammad Ayan'
+const dob = '09/12/2004'
+
+
+function sayName() {
+    console.log("hu");
+    
+    
+    console.log('My name is Mohammad ayan')
+}
