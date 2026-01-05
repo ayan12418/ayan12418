f4e223d411a9cba5ada051d6bb572f27badb0ccd
Mohammad ayan <ayanansari745@gmail.com>
2026-01-05 18:43:34 +0530

    ABC commit

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ const dob = '09/12/2004'
 
 
 function sayName() {
-    console.log("My name is ' + name1")
-    console.log("my name is ' + name2")
+    console.log("my name is + name1");
 
-    
 }
 
-function isAbove18() {
-    return age >= 18;
+function newFn() {
+    console.log("hey i am a new fn");
 }
